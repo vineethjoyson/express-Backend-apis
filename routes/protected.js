88de3f3902dfed79a7d91939da684c4ee7ad797f
vineeth <vineethjoyson4@gmail.com>
2025-08-24 15:20:10 +0530

    updated api details

--- routes/protected.js
+++ routes/protected.js
@@ -2,10 +2,13 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-
+const User = require("../models/User");
 // Protected route example
-router.get("/data", auth, (req, res) => {
-  res.json({ message: "Protected data", userId: req.user.id });
+router.post("/data", auth, async (req, res) => {
+  const { emailId } = req.body;
+  console.log(req.body);
+  const user = await User.findOne({ emailId });
+  res.json(user);
 });
 
 module.exports = router;
